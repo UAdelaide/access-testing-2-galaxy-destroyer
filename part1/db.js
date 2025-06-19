44ef977996bdc32727cdac919e421d1f083ee5a7
joshua <a1922857@R910B9E6>
2025-06-19 20:26:12 +0000

    [automatic commit] 2025-06-19 20:26:12 UTC 203.63.63.42

--- part1/db.js
+++ part1/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
     socketPath: '/var/run/mysqld/mysqld.sock',
     host: '127.0.0.1', // was localhost before
     user: 'root',
-    password:'root',
+    password: 'root',
     database: 'bookMarketplace'
 });
 
