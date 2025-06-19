20b85457b615d5cff69870e4e65f43a9aa368a5e
joshua <a1922857@R910B9E6>
2025-06-19 21:51:30 +0000

    [automatic commit] 2025-06-19 21:51:30 UTC 203.63.63.42

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -21,8 +21,7 @@ router.get('/items', async(req,res) => {
 router.post('/messages', async (req,res) => {
     const {bookID, message} = req.body;
 
-    await db.query(`INSERT INTO messages (buyerID, sellerID, bookID, messageData, timeSent)
-            VALUES (? ? ? ? NOW())`,
+    await db.query(`INSERT INTO messages (buyerID, sellerID, bookID, messageData, timeSent) VALUES (? ? ? ? NOW())`,
             [CURRENT_BUYER_ID, CURRENT_SELLER_ID, bookID,message]
         );
         res.status(201).json({ message: 'Sent message. '});
