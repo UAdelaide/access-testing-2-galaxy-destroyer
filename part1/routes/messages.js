6b1e581661b12c9d98d4c2210fab6470afc82583
joshua <a1922857@R910B9E6>
2025-06-19 22:31:56 +0000

    [automatic commit] 2025-06-19 22:31:56 UTC 203.63.63.42

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -3,8 +3,6 @@ var router = express.Router();
 var db = require('../db');
 
 var CURRENT_BUYER_ID = 1;
-var CURRENT_SELLER_ID = 2;
-
 
 // routes to create for api: 1. for items, 2. post messages, 3. get messages
 
@@ -16,15 +14,6 @@ router.get('/items', async(req,res) => {
         JOIN users u ON bl.sellerID = u.userID`
     );
     res.json(rows);
-});
-
-router.post('/messages', async (req,res) => {
-    const {bookID, sellerID, message} = req.body;
-
-    await db.query(`INSERT INTO messages (buyerID, sellerID, bookID, messageData, timeSent) VALUES (?, ?, ?, ?, NOW())`,
-            [CURRENT_BUYER_ID, sellerID, bookID,message] // this ensures we are actually sending message to current buyer
-        );
-        res.status(201).json({ message: 'Sent message. '});
 });
 
 router.get('/messages', async (req,res) => {
@@ -39,6 +28,15 @@ router.get('/messages', async (req,res) => {
         [CURRENT_BUYER_ID]
     );
     res.json(rows);
+});
+
+router.post('/messages', async (req,res) => {
+    const {bookID, sellerID, message} = req.body;
+
+    await db.query(`INSERT INTO messages (buyerID, sellerID, bookID, messageData, timeSent) VALUES (?, ?, ?, ?, NOW())`,
+            [CURRENT_BUYER_ID, sellerID, bookID,message] // this ensures we are actually sending message to current buyer
+        );
+        res.status(201).json({ message: 'Sent message. '});
 });
 
 module.exports = router;
