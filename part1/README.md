91dd85f52f775b6cf8cdd6b3fd994850b271fcc9
joshua <a1922857@R910B9E6>
2025-06-20 04:17:29 +0930

    done 1.6

--- part1/README.md
+++ part1/README.md
@@ -1,1 +1,5 @@
 Put your files for part1 here 
+
+1.5: 
+SELECT b.title, b.author, p.purchasedOn FROM purchases p JOIN bookstatus bl ON p.bookID = bl.bookID JOIN bookinfo b ON bl.bookinfoID = b.bookinfoID WHERE p.buyerID = 1 AND p.purchasedOn >= CURDATE() - INTERVAL 30 DAY ORDER BY p.purchasedOn DESC;
+
---
+++ part1/books.sql
@@ -0,0 +1,170 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
+--
+-- Host: localhost    Database: marketplace
+-- ------------------------------------------------------
+-- Server version	8.0.42-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bookinfo`
+--
+
+DROP TABLE IF EXISTS `bookinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookinfo` (
+  `bookinfoID` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(100) NOT NULL,
+  `ISBN` varchar(20) NOT NULL,
+  PRIMARY KEY (`bookinfoID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookinfo`
+--
+
+LOCK TABLES `bookinfo` WRITE;
+/*!40000 ALTER TABLE `bookinfo` DISABLE KEYS */;
+INSERT INTO `bookinfo` VALUES (1,'intro to algorithms','Thomas C','0987654344');
+/*!40000 ALTER TABLE `bookinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bookstatus`
+--
+
+DROP TABLE IF EXISTS `bookstatus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookstatus` (
+  `bookID` int NOT NULL AUTO_INCREMENT,
+  `bookinfoID` int NOT NULL,
+  `sellerID` int NOT NULL,
+  `price` float NOT NULL,
+  `bookCondition` varchar(50) NOT NULL,
+  `listedOn` date NOT NULL,
+  PRIMARY KEY (`bookID`),
+  KEY `bookinfoID` (`bookinfoID`),
+  KEY `sellerID` (`sellerID`),
+  CONSTRAINT `bookstatus_ibfk_1` FOREIGN KEY (`bookinfoID`) REFERENCES `bookinfo` (`bookinfoID`),
+  CONSTRAINT `bookstatus_ibfk_2` FOREIGN KEY (`sellerID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookstatus`
+--
+
+LOCK TABLES `bookstatus` WRITE;
+/*!40000 ALTER TABLE `bookstatus` DISABLE KEYS */;
+INSERT INTO `bookstatus` VALUES (1,1,1,69.99,'Fair','2025-06-19');
+/*!40000 ALTER TABLE `bookstatus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `locations` (
+  `locationID` int NOT NULL AUTO_INCREMENT,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  PRIMARY KEY (`locationID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'Bangalore','KA','India');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchases`
+--
+
+DROP TABLE IF EXISTS `purchases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchases` (
+  `purchaseID` int NOT NULL AUTO_INCREMENT,
+  `buyerID` int NOT NULL,
+  `bookID` int NOT NULL,
+  `purchasedOn` date NOT NULL,
+  PRIMARY KEY (`purchaseID`),
+  KEY `buyerID` (`buyerID`),
+  KEY `bookID` (`bookID`),
+  CONSTRAINT `purchases_ibfk_1` FOREIGN KEY (`buyerID`) REFERENCES `users` (`userID`),
+  CONSTRAINT `purchases_ibfk_2` FOREIGN KEY (`bookID`) REFERENCES `bookstatus` (`bookID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchases`
+--
+
+LOCK TABLES `purchases` WRITE;
+/*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
+INSERT INTO `purchases` VALUES (1,1,1,'2025-06-19');
+/*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `locationID` int NOT NULL,
+  PRIMARY KEY (`userID`),
+  KEY `locationID` (`locationID`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`locationID`) REFERENCES `locations` (`locationID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Patrick Kachappilly','patti@gmail.com','hashedpassword123',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-19 18:54:16
